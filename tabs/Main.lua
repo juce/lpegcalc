4a566a27638df8fd75e08b06f29d04682a95fea8
denisik at codea-scm <denisik@codea-scm.aws.mapote.com>
2015-11-27 19:43:39 -0500

    editing support for math functions

--- tabs/Main.lua
+++ tabs/Main.lua
@@ -9,11 +9,14 @@ function setup()
     print("Lpeg Calculator")
     
     expr, tree, res = "", "", ""
-    parameter.action("eval", function()
+    parameter.action("edit", function()
         if not isKeyboardShowing() then
             tree, res = "", ""
             showKeyboard()
         end
+    end)
+    parameter.action("clear", function()
+        expr, tree, res = "", "", ""
     end)
 end
     
@@ -22,6 +25,8 @@ local white = lpeg.S(" \t\r\n") ^ 0
 local number = white * re.compile("[0-9]+([.][0-9]+)?") / tonumber
 local muldiv = white * lpeg.C(lpeg.S("/*"))
 local addsub = white * lpeg.C(lpeg.S("+-"))
+local comma = white * lpeg.S(",")
+local ident = white * lpeg.C(re.compile("[a-z_][a-z_0-9]*"))
 local OpenParen = white * lpeg.S("(")
 local CloseParen = white * lpeg.S(")")
 
@@ -30,7 +35,9 @@ local grammar = lpeg.P({
     input = lpeg.V("exp") * -1,
     exp = lpeg.Ct(lpeg.V("term") * (addsub * lpeg.V("term"))^1) + lpeg.V("term"),
     term = lpeg.Ct(lpeg.V("factor") * (muldiv * lpeg.V("factor"))^1) + lpeg.V("factor"),
-    factor = number + OpenParen * lpeg.V("exp") * CloseParen,
+    factor = number + lpeg.V("fcall") + OpenParen * lpeg.V("exp") * CloseParen,
+    fcall = lpeg.Ct(ident * OpenParen * 
+        (lpeg.V("exp") * (comma * lpeg.V("exp"))^0)^-1 * CloseParen),
 })
 
 -- This function gets called once every frame
@@ -43,6 +50,7 @@ function draw()
 
     -- Do your drawing here
     fontSize(30)
+    textWrapWidth(WIDTH-30)
     fill(34, 255, 0, 255)
     if isKeyboardShowing() then
         text(tostring(expr) .. utf8.char(0x258c), WIDTH/2, HEIGHT/4*3)
@@ -79,6 +87,18 @@ function evaluate(expr)
     local function ev(n)
         if type(n) == "number" then
             return n
+        end
+        local first = n[1]
+        if type(first) == "string" then
+            local t = {}
+            for i=2,#n do
+                t[#t+1] = ev(n[i])
+            end
+            local f = math[first]
+            if not f then
+                error("Unknown math function: " .. first)
+            end
+            return f(unpack(t))
         end
         local acc = ev(n[1])
         for i = 2, #n, 2 do
